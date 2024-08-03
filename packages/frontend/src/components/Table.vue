8d0fbf8e0a93600517dee87fe6514ddc1a5fad5a
greeneley <anhchangtoanhoc97@gmail.com>
2024-08-03 16:14:08 +0700

    feat: update faticon and css table

--- packages/frontend/src/components/Table.vue
+++ packages/frontend/src/components/Table.vue
@@ -1,18 +1,39 @@
 <script setup>
 import items from '../data/table.json'
+
 const headers = [
-  { text: 'Loại vàng (VND/Lượng)', value: 'label', width: 200 },
+  { text: 'Loại vàng (VND/Lượng)', value: 'label', width: 400 },
   { text: 'Mua vào', value: 'buy', width: 150 },
   { text: 'Bán ra', value: 'sell', width: 150 }
 ]
+
+const onCustomBodyRow = (item, rowNumber) => {
+  return 'row'
+}
 </script>
 
 <template>
-  <EasyDataTable :headers="headers" :items="items" hide-footer alternating />
+  <EasyDataTable
+    :headers="headers"
+    :items="items"
+    hide-footer
+    alternating
+    :body-row-class-name="onCustomBodyRow"
+  />
 </template>
 
 <style>
+.header {
+  height: 50px;
+  font-size: 16px;
+}
+
 .header-text {
   font-weight: 700;
+}
+
+.row {
+  --easy-table-body-row-height: 50px;
+  --easy-table-body-row-font-size: 16px;
 }
 </style>
