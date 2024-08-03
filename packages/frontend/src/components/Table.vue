d68926905ad6941c06105b63d10b9ca86dfb7042
greeneley <anhchangtoanhoc97@gmail.com>
2024-08-03 10:57:01 +0700

    feat: add data json and refactor table component

--- packages/frontend/src/components/Table.vue
+++ packages/frontend/src/components/Table.vue
@@ -1,34 +1,14 @@
 <script setup>
+import items from '../data/table.json'
 const headers = [
   { text: 'Loại vàng (VND/Lượng)', value: 'label', width: 200 },
   { text: 'Mua vào', value: 'buy', width: 150 },
   { text: 'Bán ra', value: 'sell', width: 150 }
 ]
-
-const items = [
-  {
-    id: 1,
-    label: 'AVPL/SJC HCM',
-    buy: 77000,
-    sell: 79000
-  },
-  {
-    id: 2,
-    label: 'Hà Nội PNJ',
-    buy: 75700,
-    sell: 77020
-  },
-  {
-    id: 3,
-    label: 'SJC 1L, 10L, 1KG',
-    buy: 77000,
-    sell: 79000
-  },
-]
 </script>
 
 <template>
-  <EasyDataTable :headers="headers" :items="items" hide-footer alterning/>
+  <EasyDataTable :headers="headers" :items="items" hide-footer alternating />
 </template>
 
 <style>
