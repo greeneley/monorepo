b0060252388f0b6ee1a050f8867b09eb61f091ce
greeneley <anhchangtoanhoc97@gmail.com>
2024-08-03 10:05:30 +0700

    feat: update gitignore and add header component

---
+++ packages/frontend/src/components/Header.vue
@@ -0,0 +1,51 @@
+<template>
+  <nav id="header-top" class="navbar">
+    <div class="nav-logo">
+      <div class="container">
+        <a class="navbar-brand m-auto" href="/">GIÁ VÀNG</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style>
+#header-top {
+  background-color: #01498a;
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: 999;
+}
+
+#header-top > div {
+  width: 100%;
+  text-align: center;
+}
+
+.nav-logo {
+  border-bottom: .0625rem solid #0f599a;
+}
+
+
+.nav-logo .container {
+  position: relative;
+  width: 100%;
+  padding-left: 15px;
+  padding-right: 15px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.navbar .navbar-brand {
+  font-weight: 600;
+  font-size: 2.3rem;
+  color: white;
+}
+
+a {
+  text-decoration: none;
+  background-color: transparent;
+}
+
+</style>
--- packages/frontend/src/components/Table.vue
+++ packages/frontend/src/components/Table.vue
@@ -1,22 +1,25 @@
 <script setup>
 const headers = [
-  { text: 'Loại vàng (VND/Lượng)', value: 'label' },
-  { text: 'Mua vào', value: 'buy' },
-  { text: 'Bán ra', value: 'sell' }
+  { text: 'Loại vàng (VND/Lượng)', value: 'label', width: 200 },
+  { text: 'Mua vào', value: 'buy', width: 150 },
+  { text: 'Bán ra', value: 'sell', width: 150 }
 ]
 
 const items = [
   {
+    id: 1,
     label: 'AVPL/SJC HCM',
     buy: 77000,
     sell: 79000
   },
   {
+    id: 2,
     label: 'Hà Nội PNJ',
     buy: 75700,
     sell: 77020
   },
   {
+    id: 3,
     label: 'SJC 1L, 10L, 1KG',
     buy: 77000,
     sell: 79000
@@ -25,5 +28,11 @@ const items = [
 </script>
 
 <template>
-  <EasyDataTable :headers="headers" :items="items" />
+  <EasyDataTable :headers="headers" :items="items" hide-footer alterning/>
 </template>
+
+<style>
+.header-text {
+  font-weight: 700;
+}
+</style>
