68a34c559d3c78530ebd8db7a80672673a92b0c4
greeneley <anhchangtoanhoc97@gmail.com>
2024-08-04 10:40:47 +0700

    feat: add more feature for chart

--- packages/frontend/src/components/ChartView.vue
+++ packages/frontend/src/components/ChartView.vue
@@ -2,13 +2,57 @@
 import { ref } from 'vue'
 
 const chartOptions = ref({
+  title: {
+    text: 'Biểu đồ giá vàng SJC 1 tháng qua'
+  },
+  legend: {
+    enabled: true
+  },
+  plotOptions: {
+    line: {
+      dataLabels: {
+        enabled: true
+      }
+    }
+  },
+  rangeSelector: {
+    selected: 4,
+    enabled: false,
+    inputEnabled: false,
+    allButtonsEnabled: true,
+    buttonTheme: {
+      visibility: 'hidden'
+    },
+    labelStyle: {
+      visibility: 'hidden'
+    }
+  },
+  xAxis: {
+    minTickInterval: 24 * 3600 * 1000
+  },
   series: [
     {
-      data: [1, 2, 3]
+      name: 'Mua vào',
+      color: '#65bc00',
+      data: [
+        [1722297600000, 77000],
+        [1722211200000, 77500],
+        [1722124800000, 77500],
+        [1722038400000, 77500]
+      ]
+    },
+    {
+      name: 'Bán ra',
+      color: '#FF0000',
+      data: [
+        [1722297600000, 79000],
+        [1722211200000, 79500],
+        [1722124800000, 79500],
+        [1722038400000, 79500]
+      ]
     }
   ]
 })
-
 </script>
 
 <template>
