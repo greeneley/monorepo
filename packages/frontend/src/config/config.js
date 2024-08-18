355768d90a0697c20789e849bbf774eee50f979f
greeneley <anhchangtoanhoc97@gmail.com>
2024-08-18 09:04:07 +0700

    docker: build success

--- packages/frontend/src/config/config.js
+++ packages/frontend/src/config/config.js
@@ -2,16 +2,17 @@ const dev = {
   app: {
     base_url: 'localhost:3001'
   }
-}
+};
 
 const pro = {
   app: {
     base_url: 'XXXX' //TODO
   }
-}
+};
 
-const config = { dev, pro }
+const config = { dev, pro };
 
-const env = process.env.NODE_ENV || 'dev'
+console.log(process.env.NODE_ENV);
+const env = process.env.NODE_ENV || 'dev';
 
-module.exports = config[env]
+module.exports = config[env];
