098a1fc616f301286549debeb0a269abb4c714b9
greeneley <anhchangtoanhoc97@gmail.com>
2024-08-25 16:17:40 +0700

    frontend: automatically scroll to the top when switching between routes

--- packages/frontend/src/router/index.js
+++ packages/frontend/src/router/index.js
@@ -2,24 +2,33 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '@/views/HomePage.vue';
 import NProgress from 'nprogress';
 
+const routes = [
+  {
+    path: '/home',
+    name: 'home',
+    component: HomePage
+  },
+  {
+    path: '/',
+    redirect: '/home'
+  },
+  {
+    path: '/chart',
+    name: 'chart',
+    component: () => import('@/views/ChartPage.vue')
+  }
+];
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/home',
-      name: 'home',
-      component: HomePage
-    },
-    {
-      path: '/',
-      redirect: '/home'
-    },
-    {
-      path: '/chart',
-      name: 'chart',
-      component: () => import('@/views/ChartPage.vue')
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return { top: 0, behavior: 'smooth' };
     }
-  ]
+  }
 });
 
 router.beforeResolve((to, from, next) => {
