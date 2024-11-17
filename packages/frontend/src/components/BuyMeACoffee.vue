1ac5e43611f6c839ca4ed2436ebbbb899c6f093c
greeneley <anhchangtoanhoc97@gmail.com>
2024-11-17 22:00:44 +0700

    frontend: fix bug a hover

--- packages/frontend/src/components/BuyMeACoffee.vue
+++ packages/frontend/src/components/BuyMeACoffee.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    <div class="bmcButtonContainer">
-      <a href="https://www.buymeacoffee.com/htdinh" target="_blank"
-        ><img
-          src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
-          alt="Buy Me A Coffee"
-          style="height: 50px !important; width: 180px !important"
-      /></a>
-    </div>
+    <a href="https://www.buymeacoffee.com/htdinh" target="_blank"
+      ><img
+        src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+        alt="Buy Me A Coffee"
+        style="height: 50px !important; width: 180px !important"
+    /></a>
   </div>
 </template>
+
+<style scoped>
+a:hover {
+  background-color: transparent;
+}
+</style>
