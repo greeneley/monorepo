e623d5619d6dfc622aa96d0d4793bd22d703992c
greeneley <anhchangtoanhoc97@gmail.com>
2025-01-11 16:04:22 +0700

    Update gold service

--- packages/backend-nestjs/src/gold/gold.service.ts
+++ packages/backend-nestjs/src/gold/gold.service.ts
@@ -6,44 +6,52 @@ import { AxiosError } from 'axios';
 @Injectable()
 export class GoldService {
   private readonly logger = new Logger(GoldService.name);
+
   constructor(private readonly httpService: HttpService) {}
+
   async findAll(): Promise<any> {
-    const { data } = await firstValueFrom(
-      this.httpService.get('https://gw.vnexpress.net/cr/?name=tygia_vangv202206').pipe(
-        catchError((error: AxiosError) => {
-          this.logger.error(error.response.data);
-          throw 'An error happend';
-        })
-      )
-    );
-    const goldData = data?.data?.data?.gold;
-    const newGoldItems = goldData?.new;
-    const oldGoldItems = goldData?.old;
-    const listGoldTypes = Object.keys(newGoldItems);
-    console.log(listGoldTypes);
+    try {
+      const { data } = await firstValueFrom(
+        this.httpService.get('https://gw.vnexpress.net/cr/?name=tygia_vangv202206')
+      );
 
-    return listGoldTypes.map((goldType, index) => {
-      const detailNewGold = newGoldItems[goldType];
-      const detailOldGold = oldGoldItems[goldType];
+      const goldData = data?.data?.data?.gold;
+      if (!goldData) {
+        this.logger.error('Gold data is not available');
+        throw new Error('Gold data is not available');
+      }
 
-      const currentBuyPrice = detailNewGold.buy * 1000;
-      const currentSellPrice = detailNewGold.sell * 1000;
+      const { new: newGoldItems, old: oldGoldItems } = goldData;
+      const listGoldTypes = Object.keys(newGoldItems);
 
-      const buyDifference = (detailNewGold.buy - detailOldGold.buy) * 1000;
-      const sellDifference = (detailNewGold.sell - detailOldGold.sell) * 1000;
+      return listGoldTypes.map((goldType, index) => this.mapGoldType(goldType, index, newGoldItems, oldGoldItems));
+    } catch (error) {
+      this.logger.error(error instanceof AxiosError ? error.response.data : error.message);
+      throw new Error('An error happened while fetching gold data');
+    }
+  }
 
-      const currentDifference = (detailNewGold.sell - detailNewGold.buy) * 1000;
+  private mapGoldType(goldType: string, index: number, newGoldItems: any, oldGoldItems: any) {
+    const detailNewGold = newGoldItems[goldType];
+    const detailOldGold = oldGoldItems[goldType];
 
-      return {
-        id: index,
-        type: goldType,
-        label: detailNewGold.label,
-        buyPrice: currentBuyPrice,
-        sellPrice: currentSellPrice,
-        buyDifference,
-        sellDifference,
-        currentDifference
-      };
-    });
+    const currentBuyPrice = detailNewGold.buy * 1000;
+    const currentSellPrice = detailNewGold.sell * 1000;
+
+    const buyDifference = (detailNewGold.buy - detailOldGold.buy) * 1000;
+    const sellDifference = (detailNewGold.sell - detailOldGold.sell) * 1000;
+
+    const currentDifference = (detailNewGold.sell - detailNewGold.buy) * 1000;
+
+    return {
+      id: index,
+      type: goldType,
+      label: detailNewGold.label,
+      buyPrice: currentBuyPrice,
+      sellPrice: currentSellPrice,
+      buyDifference,
+      sellDifference,
+      currentDifference
+    };
   }
 }
