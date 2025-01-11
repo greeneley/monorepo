ad61b3f17abe445f74b56168aa9746edfd5172aa
greeneley <anhchangtoanhoc97@gmail.com>
2025-01-11 15:58:52 +0700

    backup

--- packages/backend-nestjs/src/gold/gold.service.ts
+++ packages/backend-nestjs/src/gold/gold.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { catchError, firstValueFrom, Observable } from 'rxjs';
+import { catchError, firstValueFrom } from 'rxjs';
 import { AxiosError } from 'axios';
 
 @Injectable()
@@ -22,23 +22,27 @@ export class GoldService {
     const listGoldTypes = Object.keys(newGoldItems);
     console.log(listGoldTypes);
 
-    const result = [];
-    listGoldTypes.forEach((goldType) => {
+    return listGoldTypes.map((goldType, index) => {
       const detailNewGold = newGoldItems[goldType];
       const detailOldGold = oldGoldItems[goldType];
 
-      const buyNew = detailNewGold.buy;
-      const buyOld = detailOldGold.buy;
-    });
+      const currentBuyPrice = detailNewGold.buy * 1000;
+      const currentSellPrice = detailNewGold.sell * 1000;
 
-    return Object.keys(newGoldItems).map((goldItem, index) => {
-      const detailGoldItem = newGoldItems[goldItem];
+      const buyDifference = (detailNewGold.buy - detailOldGold.buy) * 1000;
+      const sellDifference = (detailNewGold.sell - detailOldGold.sell) * 1000;
+
+      const currentDifference = (detailNewGold.sell - detailNewGold.buy) * 1000;
+
       return {
         id: index,
-        type: goldItem,
-        label: detailGoldItem.label,
-        buyPrice: detailGoldItem.buy,
-        sellPrice: detailGoldItem.sell
+        type: goldType,
+        label: detailNewGold.label,
+        buyPrice: currentBuyPrice,
+        sellPrice: currentSellPrice,
+        buyDifference,
+        sellDifference,
+        currentDifference
       };
     });
   }
